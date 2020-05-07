13fd7294ee076123dad421f12bdd695ed71fb0bd
shreypithava <shreygalaxy3@gmail.com>
2020-05-07 12:39:44 -0500

    database variable not global anymore, functionality to change only notes

--- companies.py
+++ companies.py
@@ -3,8 +3,6 @@ import time
 
 
 class Companies:
-    def __init__(self):
-        self.__db = None
 
     def start(self):
         choice = 0
@@ -19,64 +17,73 @@ class Companies:
                                 "('get out' to go back to menu)\n")
                 if company == 'get out':
                     continue
-                self.__db = sqlite3.connect('companies.db')
                 self.__add(company)
-                self.__db.commit()
-                self.__db.close()
             elif choice == 2:
-                self.__show_records()
+                self.__show_records(sqlite3.connect('companies.db'))
             elif choice == 3:
                 month = float(input("How many months ago?\n"))
-                self.__db = sqlite3.connect('companies.db')
+                db = sqlite3.connect('companies.db')
                 query = """Select * from Companies where epoch_time < {}
                 ORDER BY epoch_time DESC""".format(
                     int(time.time() - 2_592_000 * month))
-                records = self.__db.execute(query)
+                records = db.execute(query)
                 for record in records:
                     print(record)
-                self.__db.close()
+                db.close()
             elif choice == 9:
                 break
             else:
                 print('Enter correct response')
 
-    def __show_records(self):
-        specific = input("Specific?\n")
-        self.__db = sqlite3.connect('companies.db')
+    @staticmethod
+    def __show_records(db, specific=None):
+        if specific is None:
+            specific = input("Specific?\n")
         if specific.upper() == 'NO':
-            for company in self.__db.execute('''select * from Companies
+            for company in db.execute('''select * from Companies
             order by epoch_time DESC'''):
                 print(company)
         elif specific.isdigit():
-            for company in self.__db.execute('''select * from Companies
+            print('\nShowing last {} companies\n'.format(specific))
+            for company in db.execute('''select * from Companies
             order by epoch_time DESC LIMIT ?''', (int(specific),)):
                 print(company)
         else:
             query = """SELECT * FROM Companies
             WHERE CompanyID LIKE '%{}%'""".format(specific)
-            specific_companies = self.__db.execute(query)
+            specific_companies = db.execute(query)
             for company in specific_companies:
                 print(company)
-        self.__db.close()
 
     def __add(self, company):
+        db = sqlite3.connect('companies.db')
+        self.__show_records(db, company)
+        company_entered = input("Enter existing company or new company\n")
         notes = input("Enter notes\n")
         try:
-            self.__db.execute('pragma foreign_keys = on')
-            self.__db.execute("""INSERT into Companies values
+            db.execute('pragma foreign_keys = on')
+            db.execute("""INSERT into Companies values
         (?, datetime('now', 'localtime'), ?, ?)""",
-                              (company, int(time.time()), notes))
+                       (company_entered, int(time.time()), notes))
         except sqlite3.OperationalError:
             print("SQL error, company not added")
         except sqlite3.IntegrityError:
-            self.__update(company, notes)
+            only_notes = input("Edit only notes?\n")
+            self.__update(company_entered, notes, only_notes, db)
         else:
             print("Insert successful")
+        # self.__db.commit()
+        db.close()
 
-    def __update(self, company, new_notes):
-        self.__db.execute("""UPDATE Companies set Last_Updated = datetime('now', 'localtime'),
-        epoch_time = ?, Notes = ? where CompanyID = ?""",
-                          (int(time.time()), company, new_notes))
+    @staticmethod
+    def __update(company, new_notes, only_notes, db):
+        if only_notes == 'n':
+            db.execute("""UPDATE Companies set Last_Updated = datetime('now', 'localtime'),
+            epoch_time = ?, Notes = ? where CompanyID = ?""",
+                       (int(time.time()), company, new_notes))
+        else:
+            db.execute("""UPDATE Companies set
+            Notes = ? where CompanyID = ?""", (new_notes, company))
         print("Update successful")
 
 
